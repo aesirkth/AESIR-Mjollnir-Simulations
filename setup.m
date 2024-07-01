54166f87850e4ab897a87114c9b4c961ddb175e0
spiggen <jonasvilgotlotberg@hotmail.se>
2024-07-01 17:26:24 +0200

    fixed python and coolprop issue, and added a installer for conda, python and coolprop

--- setup.m
+++ setup.m
@@ -2,9 +2,35 @@ clc
 clear
 clear global
 
+
+%% Adding paths to sub-folders:
 addpath('./Datasets');
 addpath('./Simulation')
 addpath('./Simulation/Combustion');
 addpath('./Simulation/Flight');
 addpath('./Simulation/Thrust');
 addpath('./Plots');
+
+
+%% Python-setup:
+mypath = pwd(); 
+mypath = split(mypath, "\"); 
+mypath = mypath{1}+"\"+mypath{2}+"\"+mypath{3}; % Oooga booga
+
+
+pyenv('Version', mypath+'\miniconda3\envs\matlab_python_enviroment\python.exe')
+disp("Loading CoolProp into MATLAB:")
+try
+py.importlib.import_module("CoolProp");
+test = py.CoolProp.CoolProp.PropsSI('D', 'T', 200, 'Q', 0, 'NitrousOxide');
+disp("test:"+string(test));
+disp("Package loaded correctly.")
+catch
+disp("Loading of CoolProp package failed. Run installer.bat to install CoopProp and python.")
+end
+
+
+
+
+
+
