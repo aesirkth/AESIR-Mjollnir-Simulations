804ead91a5c6e572d75d40e4e246fe7ef12b243c
spiggen <jonasvilgotlotberg@hotmail.se>
2024-10-09 15:55:39 +0200

    archived stuff

---
+++ Archive/Old_engine_sim/setup.m
@@ -0,0 +1,10 @@
+clc
+clear
+clear global
+
+addpath('./Datasets');
+addpath('./Simulation')
+addpath('./Simulation/Combustion');
+addpath('./Simulation/Flight');
+addpath('./Simulation/Thrust');
+addpath('./Plots');
