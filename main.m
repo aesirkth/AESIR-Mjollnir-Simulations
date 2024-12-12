76d18f0780fd61e6809d7b3a5d3963989c1d4990
spiggen <jonasvilgotlotberg@hotmail.se>
2024-12-12 22:38:27 +0100

    should now be working

--- main.m
+++ main.m
@@ -1,21 +1,16 @@
 setup;
 
-my_rocket = mjollnir_recursive;
+my_rocket = mjollnir;
 
 
 disp("Simulating...")
 job = struct(); job.t_max = 100;
 sim = run_simulation(my_rocket);
 disp("Done.")
-%sim.rocket_historian.position(:,:) = 0;
 
 
+%delete(".\Data\recursive_test.txt")
+%struct2txt(".\Data\recursive_test.txt", sim.rocket_historian, 0:1/30:100);
 
-delete(".\Data\recursive_test.txt")
-struct2txt(".\Data\recursive_test.txt", sim.rocket_historian, 0:1/30:100);
 
-
-render_simulation_recursive(sim)
-%disp("Rendering...")
-%render_job = render_simulation(sim);
-%disp("Done.")
+render_simulation(sim)
